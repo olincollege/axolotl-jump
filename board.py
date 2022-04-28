8e831c58961dec25d4b0f94a7ac6193b9c3f1a30
Jen Lee <jlee7@olin.edu>
2022-04-28 13:16:33 -0400

    Board and Character files first edit

--- board.py
+++ board.py
@@ -1,5 +1,46 @@
-# Class Character
-#     Init
-#         Location
-#         Dead or alive
-#         Sprite
+import pygame
+from pygame.locals import *
+
+scr_size = (width,height) = (600,600)
+FPS = 60
+gravity = 0.6
+
+screen = pygame.display.set_mode(scr_size)
+clock = pygame.time.Clock()
+pygame.display.set_caption("Mushroom Run")
+
+class Map():
+    def __init__(self):
+        self.isDead = False
+        self.surf = pygame.Surface(())
+        self.surf.fill(())
+        self.rect = self.surf.get_rect(center = (10,420))   
+
+class Ground():
+    def __init__(self, speed = -5):
+        self.surf = pygame.Surface(())
+        self.surf.fill(())
+        self.rect = self.surf.get_rect(center = (10,420))   
+        self.speed = speed
+
+    def draw(self):
+        screen.blit(self.rect)
+
+    def update(self):
+        self.rect += self.speed
+
+class Obstacles(pygame.sprite.Sprite):
+    def __init__(self):
+        self.surf = pygame.Surface(())
+        self.surf.fill(())
+        self.rect = self.surf.get_rect(center = (10,420))
+        self.movement = [-1*speed, 0]
+
+    def draw(self):
+        screen.blit(self.rect)
+
+    def update(self):
+        self.rect = self.rect.move(self.movement)
+
+        if self.rect < 0:
+            self.kill()
